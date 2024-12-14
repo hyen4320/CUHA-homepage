c1b6d1554fea465cf659f69f79c75b7f525d8e59
king_ing_e <2022011870@cju.ac.kr>
2024-12-14 19:08:09 +0900

    starter pack v1.0.1-user_clear

--- src/main/java/CUHA/homepage/controller/ExamController.java
+++ src/main/java/CUHA/homepage/controller/ExamController.java
@@ -1,8 +1,9 @@
 package CUHA.homepage.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/exam")
 public class ExamController {
 }
